7e00fc197a331b7a0ad8ce7c3d996a39b5c31a5b
Biray <games.biray@gmail.com>
2025-09-18 23:09:53 +0300

    added calculator and get file info

---
+++ functions/get_files_info.py
@@ -0,0 +1,24 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    abs_working_dir = os.path.abspath(working_directory)
+    full_dir = os.path.join(working_directory, directory)
+    full_path = os.path.abspath(full_dir)
+
+    if not full_path.startswith(abs_working_dir):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+    if not os.path.isdir(full_path):
+        return f'Error: "{directory}" is not a directory'
+
+    files_in_dir = os.listdir(full_path)
+    files_info = []
+    try:
+        for file in files_in_dir:  
+            file_path = os.path.join(full_path, file)
+            is_dir = os.path.isdir(file_path)
+            file_size = os.path.getsize(file_path)
+            files_info.append(f"- {file}: file_size={file_size} bytes, is_dir={is_dir}")
+        return "\n".join(files_info)
+    except Exception as e:
+        return f"Error: {e}"
