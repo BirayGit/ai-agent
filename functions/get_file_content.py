ebb464e3116b2d37a7703f88143092c5a130b624
Biray <games.biray@gmail.com>
2025-09-25 15:11:34 +0300

    added get file content and tests for it

---
+++ functions/get_file_content.py
@@ -0,0 +1,21 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    full_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not full_path.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.isfile(full_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    try:
+        with open(full_path, "r") as f:
+            file_content = f.read(MAX_CHARS)
+            if f.read(1):
+                file_content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        return file_content
+    except Exception as e:
+        return f'Error reading file "{file_path}": {e}'
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,8 +2,7 @@ import os
 
 def get_files_info(working_directory, directory="."):
     abs_working_dir = os.path.abspath(working_directory)
-    full_dir = os.path.join(working_directory, directory)
-    full_path = os.path.abspath(full_dir)
+    full_path = os.path.abspath(os.path.join(working_directory, directory))
 
     if not full_path.startswith(abs_working_dir):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
